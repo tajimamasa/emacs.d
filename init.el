0892be8b6ed0e1de7186981e9917479166347a17
Tajima <nori@TajimanoMacBook-Air.local>
2020-11-01 01:56:30 +0900

    a

---
+++ init.el
@@ -0,0 +1,37 @@
+
+
+; ----------------
+; key bind
+; ----------------
+;; C-h
+(global-set-key "\C-h" 'delete-backward-char)
+;; help
+(global-set-key "\C-c\C-h" 'help-command)
+;; comment out
+(global-set-key "\C-c;" 'comment-dwim)
+;; window operation
+(global-set-key "\C-t" 'other-window)
+
+
+; ----
+; preferences
+; ----
+; 括弧を自動で補完する
+(electric-pair-mode 1)
+;; tabにスペース４つを利用
+(setq-default tab-width 4 indent-tabs-mode nil)
+;; デフォルトの起動時のメッセージを表示しない
+(setq inhibit-startup-message t)
+;; 列の番号
+(column-number-mode t)
+;; 行番号の表示
+(global-linum-mode t)
+;; 1行ごとの改ページ
+(setq scroll-conservatively 1)
+;; 対応する括弧を光らせる
+(show-paren-mode 1)
+;; メニューバーの非表示
+(menu-bar-mode -1)
+;; backupファイル作らない
+(setq make-backup-files nil)
+(setq auto-save-default nil)
